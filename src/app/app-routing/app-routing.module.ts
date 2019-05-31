716c3f42872893a56f28c1fa5bbcf193d3fa89c4
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-31 11:44:10 +0700

    save

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PlayerComponent } from '../player/player.component';
+import { PlayerDetailsComponent } from '../player-details/player-details.component';
+import { AddPlayerComponent } from '../add-player/add-player.component';
+
+const routes: Routes = [
+  {
+    path: 'players',
+    component: PlayerComponent
+  },
+  {
+    path: 'player/add',
+    component: AddPlayerComponent
+  },
+  {
+    path: ' ',
+    component: PlayerDetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: 'players',
+    pathMatch: 'full'
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
