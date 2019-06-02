de92dc80e8894b2dc784e336f3375ad828baa075
Tuấn Huy Đặng <huyb1505883@student.ctu.edu.vn>
2019-06-02 10:34:57 +0700

    'save'

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlayerComponent } from '../player/player.component';
 import { PlayerDetailsComponent } from '../player-details/player-details.component';
 import { AddPlayerComponent } from '../add-player/add-player.component';
+import { Route } from '@angular/compiler/src/core';
 
 const routes: Routes = [
   {
@@ -29,4 +30,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {
+  constructor(router: Route) { }
+}
